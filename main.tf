b17f2f5f2550cc7b57229ff01edf0128b1fecb25
abutkys <abutkys4@gmail.com>
2025-02-19 19:57:57 +0200

    add user data and tags to AWS instance configuration in main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,17 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "linux" {
   ami = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  user_data_replace_on_change = true
+  tags = {
+    Name = "terraform-example"
+  }      
 }
