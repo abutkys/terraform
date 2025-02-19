cd5154db370808424e3d595279178d96c74cd422
abutkys <abutkys4@gmail.com>
2025-02-19 20:27:07 +0200

    refactor: rename AWS instance resource from 'linux' to 'example'

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "linux" {
+resource "aws_instance" "example" {
   ami = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
