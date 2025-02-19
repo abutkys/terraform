bf100256e93127d1d63d0a199325f9fec9acafc1
abutkys <abutkys4@gmail.com>
2025-02-19 20:20:12 +0200

    add security group for AWS instance with ingress rules

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "linux" {
   ami = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -14,5 +15,16 @@ resource "aws_instance" "linux" {
   user_data_replace_on_change = true
   tags = {
     Name = "terraform-example"
-  }      
+  }   
+
+}
+
+resource "aws_security_group" "instance" {
+ name= "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
 }
