5725042e9ca3f9039804b8c09b8963c3ac0011c1
abutkys <abutkys4@gmail.com>
2025-03-04 15:35:28 +0200

    refactor: replace aws_instance with aws_launch_configuration and add autoscaling group

--- main.tf
+++ main.tf
@@ -2,20 +2,29 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0fb653ca2d3203ac1"
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
-  user_data_replace_on_change = true
-  tags = {
-    Name = "terraform-example"
-  }   
+
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  min_size = 5
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
 
 }
 
@@ -23,9 +32,20 @@ resource "aws_security_group" "instance" {
  name= "terraform-example-instance"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = var.server_port
+    to_port = var.server_port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"] 
   }
+}
+
+variable "server_port" {
+  description = "value of server port"
+  type = number
+  default = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public ip address of the web server"
 }
